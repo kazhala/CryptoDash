19b2a9195ce0b9caaf89e1757a7fe7e97290fdbe
kevin zhuang <kevinzhuang@192-168-1-110.tpgi.com.au>
2019-09-09 15:10:49 +1000

    style app bar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Bar = styled.div`
     display: grid;
-    grid-template-columns: 180px auto 100px 100px
+    grid-template-columns: 180px auto 100px 100px;
+    margin-bottom: 40px;
 `
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`;
+
+const ControlButtonEle = styled.div`
+    cursor: pointer;
+    ${ props => props.active && css`
+        text-shadow: 0px 0px 60px #03ff03
+    `}
+`;
+
+export const ControlButton = props => {
+    return (
+        <ControlButtonEle active={props.active}>
+            {toProperCase(props.name)}
+        </ControlButtonEle>
+    )
+}
+
+const toProperCase = lower => {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
 
 const AppBar = props => {
 
     return (
         <Bar>
-            <div>CrytoDash</div>
+            <Logo>CrytoDash</Logo>
             <div />
-            <div>DashBoard</div>
-            <div>Settings</div>
+            <ControlButton active name={"dashboard"} />
+            <ControlButton name={"settings"} />
         </Bar>
     );
 }
