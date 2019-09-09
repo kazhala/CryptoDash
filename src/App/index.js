9c45997e8ab71ef0b60e8ae20d070f1237a5a731
kevin zhuang <kevinzhuang@192-168-1-110.tpgi.com.au>
2019-09-09 14:19:54 +1000

    added app layout

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import './App.css';
+import AppLayout from './AppLayout';
 
 const MyButton = styled.div`
   color: green;
@@ -9,12 +10,12 @@ const MyButton = styled.div`
 
 function App() {
   return (
-    <div>
+    <AppLayout>
       <MyButton>
         hello
       </MyButton>
       hello world
-    </div>
+    </AppLayout>
   );
 }
 
