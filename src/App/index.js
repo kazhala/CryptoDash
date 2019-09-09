73e644fe1b7ed3c7fc8babe5ab62918396eaa36b
kevin zhuang <kevinzhuang@192-168-1-110.tpgi.com.au>
2019-09-09 14:13:36 +1000

    added google fonts do hyeon

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+
+const MyButton = styled.div`
+  color: green;
+`
 
 function App() {
   return (
     <div>
+      <MyButton>
+        hello
+      </MyButton>
       hello world
     </div>
   );
