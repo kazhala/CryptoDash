a28dd02269fa0eb2df6055653afb8f9f8d25a9e7
kevin zhuang <kevinzhuang@kevins-MacBook-Pro-3.local>
2019-09-11 12:40:44 +1000

    edit the theme of the chart

--- src/DashBoard/PriceChart.js
+++ src/DashBoard/PriceChart.js
@@ -3,6 +3,9 @@ import HighChartsConfig from './HighChartsConfig';
 import { Tile } from '../Shared/Tile';
 import { AppContext } from '../App/AppProvider';
 import ReactHighCharts from 'react-highcharts';
+import HighChartsTheme from './HighChartsTheme';
+
+ReactHighCharts.Highcharts.setOptions(HighChartsTheme);
 
 const PriceChart = props => {
     const priceContext = useContext(AppContext);
